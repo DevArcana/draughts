88081926f624344c28553167525d593a0c17bbe0
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-05-04 19:03:52 +0200

    scaffold projects

--- src/Draughts.Wasm/Program.cs
+++ src/Draughts.Wasm/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Draughts.Blazor
+namespace Draughts.Wasm
 {
     public class Program
     {
