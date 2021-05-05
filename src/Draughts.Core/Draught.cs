d32816f5e44c08ae97c103b5bae7f813ff12de19
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-05-05 22:16:39 +0200

    generate projects from scratch

--- src/Draughts.Core/Draught.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Draughts.Core
-{
-    public record Draught (int X, int Y, bool IsBlack);
-}
--- src/Draughts.Server/Startup.cs
+++ src/Draughts.Server/Startup.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Draughts.Api.Hubs;
-using Draughts.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Draughts.Api
+namespace Draughts.Server
 {
     public class Startup
     {
@@ -29,42 +26,25 @@ namespace Draughts.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Draughts.Api", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Draughts.Server", Version = "v1" });
             });
-            
-            services.AddResponseCompression(opts =>
-            {
-                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                    new[] { "application/octet-stream" });
-            });
-
-            services.AddSingleton<Draughtboards>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseResponseCompression();
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseWebAssemblyDebugging();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Draughts.Api v1"));
-            }
-            else
-            {
-                app.UseHsts();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Draughts.Server v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -73,8 +53,6 @@ namespace Draughts.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<DraughtsHub>("/api/hub");
-                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
